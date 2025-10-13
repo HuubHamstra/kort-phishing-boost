2e9dcb76c0c101ae82f4ff201cd8bbfcfca5c427
Huub <115623884+HuubHamstra@users.noreply.github.com>
2025-10-13 20:16:23 +0200

    Revert "feat: implement visitor logging functionality and create visitor_logs table"
    This reverts commit ec0feb39da9f1a9050783bc1f4860b6ff8fe08f2.

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,12 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Training from "./pages/Training";
 import NotFound from "./pages/NotFound";
-import { useEffect } from "react";
-import { logVisit } from "@/integrations/supabase/logVisit";
 
 const queryClient = new QueryClient();
-
-function RouteLogger() {
-  const location = useLocation();
-  useEffect(() => {
-    logVisit(location.pathname);
-  }, [location.pathname]);
-  return null;
-}
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
@@ -25,7 +15,6 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <RouteLogger />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/training" element={<Training />} />
